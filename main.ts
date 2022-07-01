30002068d3b76d6c124e44c0e801f72ddd1cf02d
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-07-01 12:23:20 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
-})
+
+namespace basic {
+    /**
+     *  place a number
+     */
+    //% block
+    //% group="Show value number"
+    export function useNumber(num: number): number {
+        return num
+    }
+}
